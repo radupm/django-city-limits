ac2a6942bd401f563d83d1607441f9e2f9e3e6b0
Radu M Popescu <popescu.radum@gmail.com>
2021-04-07 15:53:28 +0300

    checking license in view

--- django_limits/views.py
+++ django_limits/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 
 @login_required
 def limit_exceeded_view(request, limit_exception, template=None):
-
+    license_data = None
     if hasattr(limit_exception, "get_license_func"):
         get_license_func = limit_exception.get_license_func
         license_data = get_license_func() if get_license_func else None
