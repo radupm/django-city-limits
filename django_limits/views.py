4cb5398dc41787009b4458e9a85dc7e53f5ec586
Radu M Popescu <popescu.radum@gmail.com>
2021-04-07 16:06:44 +0300

    bugfix

--- django_limits/views.py
+++ django_limits/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render, redirect
 @login_required
 def limit_exceeded_view(request, limit_exception, template=None):
     license_data = None
-    if hasattr(limit_exception, "get_license_func"):
-        get_license_func = limit_exception.get_license_func
+    if limit_exception.details.get("get_license_func"):
+        get_license_func = limit_exception.details.get("get_license_func")
         license_data = get_license_func() if get_license_func else None
     if template is None:
         template = "django_limits/limit_exceeded.html"
@@ -17,7 +17,7 @@ def limit_exceeded_view(request, limit_exception, template=None):
             "exception": limit_exception,
             "license": license_data,
             "model_name": limit_exception.model._meta.verbose_name,
-            "model_name_plural": limit_exception.model._meta.verbose_name_plural
+            "model_name_plural": limit_exception.model._meta.verbose_name_plural,
         },
-        status=403
+        status=403,
     )
