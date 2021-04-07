6048d877d726bce65d4f45d8ce0198e6f4f6cfb2
Radu M Popescu <popescu.radum@gmail.com>
2021-04-07 15:52:40 +0300

    checking license in view

--- django_limits/views.py
+++ django_limits/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render, redirect
 
 @login_required
 def limit_exceeded_view(request, limit_exception, template=None):
+
+    if hasattr(limit_exception, "get_license_func"):
+        get_license_func = limit_exception.get_license_func
+        license_data = get_license_func() if get_license_func else None
     if template is None:
         template = "django_limits/limit_exceeded.html"
     return render(
@@ -11,6 +15,7 @@ def limit_exceeded_view(request, limit_exception, template=None):
         template,
         {
             "exception": limit_exception,
+            "license": license_data,
             "model_name": limit_exception.model._meta.verbose_name,
             "model_name_plural": limit_exception.model._meta.verbose_name_plural
         },
